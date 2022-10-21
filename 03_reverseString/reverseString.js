0f71ae9bee0db32085a0dcc730d61ad6e22fbea2
tsheldon11 <tasheldon90@gmail.com>
2022-10-21 21:41:33 +0100

    Completed exercises 1-4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let items = Array.from(string);
+    let reverseItem = "";
+
+    for(let i=items.length-1; i>=0; i--){
+        reverseItem += items[i]; 
+    }
+
+    return reverseItem;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...theArgs) {
+    let firstArray = Array.from(arguments[0]);
 
+    for(let i=firstArray.length; i>0; i--){
+        for(const item of firstArray){
+            if(item === arguments[i]){
+                firstArray.splice(firstArray.indexOf(item),1);
+            }
+        }
+    }
+    return firstArray;
 };
 
 // Do not edit below this line
