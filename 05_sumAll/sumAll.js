be1c1db57f5b43327c5d74a421c57ff9c3a51906
tsheldon11 <tasheldon90@gmail.com>
2022-10-23 01:58:01 +0100

    Solved Activity 5 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(intOne, intTwo) {
+    let total = 0;
+    let i = 0;
+    let x = 0;
 
+    if(intOne < 0 || intTwo < 0 || !Number.isInteger(intOne) || !Number.isInteger(intTwo)){
+        return "ERROR";
+    }
+
+    if(intOne<intTwo){
+        i = intOne;
+        x = intTwo;
+    } else {
+        i = intTwo;
+        x = intOne;
+    }
+
+    for(i; i <=x; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
