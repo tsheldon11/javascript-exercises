98c457b3e89a05f7ed9aab0b11d38be53dc9626c
tsheldon11 <tasheldon90@gmail.com>
2022-10-23 15:32:32 +0100

    Solved Activities 6 and 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let newC = (tempF-32) * 5/9;
+  return Math.round(newC * 10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let newF = tempC * 9/5 + 32;
+  return Math.round(newF *10)/10;
 };
 
 // Do not edit below this line
